450ee5fa53556e0337ea7199645c9fe606e9ea87
itblock <wnsuddlsla@gmail.com>
2019-02-01 10:50:04 +0900

    Query load by files.

--- inst/extdata/Gene_setting.sql
+++ inst/extdata/Gene_setting.sql
@@ -1,6 +1,4 @@
-SELECT B.target_gene_source_value, A.variant_exon_number,
-        A.hgvs_p, A.sequence_alteration, A.variant_feature,
-        C.variant_origin, C.variant_pathogeny
+SELECT B.target_gene_source_value
         FROM (SELECT * FROM @schema.dbo.variant_occurrence
         WHERE person_id IN (SELECT subject_id FROM @schema.dbo.@Cohort_table)) A
         LEFT OUTER JOIN @schema.dbo.[target_gene] B ON A.target_gene_id = B.target_gene_concept_id
